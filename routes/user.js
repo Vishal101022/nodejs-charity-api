eac147a101071fcf14903579156b8982350b96a6
Vishal101022 <raivishal939@gmail.com>
2024-11-20 00:48:51 +0530

    Add login, users routes

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const user = require("../controllers/user");
+const auth = require("../middleware/auth");
 
 router.post("/register", user.register);
+router.post("/login", user.login);
+router.get("/users", auth.authMiddleware, user.getUser);
+router.get("/users/:id", auth.authMiddleware, user.getUserById);
+router.patch("/user", auth.authMiddleware, user.updateUser);
 
 module.exports = router;
