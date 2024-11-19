5799aff1b1739c8e1f5adbf1346eaca7abc24cd9
Vishal101022 <raivishal939@gmail.com>
2024-11-19 17:35:31 +0530

    :sparkles: create user model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require("../util/db");
+
+const User = sequelize.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: true}
+);
+
+module.exports = User;
