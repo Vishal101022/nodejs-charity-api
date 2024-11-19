6427306e1adadb04f30c1a2e16c5874fc617c26b
Vishal101022 <raivishal939@gmail.com>
2024-11-20 03:14:57 +0530

    Add attributes to fetch column

--- controllers/user.js
+++ controllers/user.js
@@ -24,45 +24,55 @@ exports.register = async (req, res) => {
       isAdmin,
       password: hashedPassword,
     });
-    res.status(201).json({ message: "User created successfully"});
+    res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-
 exports.login = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-      // check user exists or not
-      const user = await User.findOne({ where: { email } });
-      if (!user) {
-        return res.status(404).json({ error: "User not found" });
-      }
-        // compare password
-      const passwordMatch = await bcrypt.compare(password, user.password);
-      if (!passwordMatch) {
-        return res.status(401).json({ error: "Invalid credentials" });
-      }
-      // generate JWT token
-      const token = jwt.sign({ userId: user.id , name: user.name}, process.env.JWT_SECRET, {
-        expiresIn: "24h",
-      });
-
-      // return token
-      return res.status(200).json({ message: "Login successful", token });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+  const { email, password } = req.body;
+  try {
+    // check user exists or not
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
+    // compare password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    // generate JWT token
+    const token = jwt.sign(
+      { userId: user.id, name: user.name },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "24h",
+      }
+    );
+
+    // return token
+    return res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 // get user details
 exports.getUser = async (req, res) => {
   try {
-    const user = await User.findByPk(req.user.id
-      , {
-        attributes: ["id", "name", "email", "phone", "address", "isAdmin", "createdAt"],}
-    );
+    const user = await User.findByPk(req.user.id, {
+      attributes: [
+        "id",
+        "name",
+        "email",
+        "phone",
+        "address",
+        "isAdmin",
+        "createdAt",
+      ],
+    });
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
@@ -74,11 +84,11 @@ exports.getUser = async (req, res) => {
 
 // update user details
 exports.updateUser = async (req, res) => {
-   const  { name, phone, address, isAdmin } = req.body;
+  const { name, phone, address, isAdmin } = req.body;
   try {
     const user = await User.findByPk(req.user.id);
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(401).json({ error: "unauthorized" });
     }
 
     user.name = name || user.name;
@@ -91,12 +101,22 @@ exports.updateUser = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+};
 
 // get user details with id
 exports.getUserById = async (req, res) => {
   try {
-    const user = await User.findByPk(req.params.id);
+    const user = await User.findByPk(req.params.id, {
+      attributes: [
+        "id",
+        "name",
+        "email",
+        "phone",
+        "address",
+        "isAdmin",
+        "createdAt",
+      ],
+    });
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
