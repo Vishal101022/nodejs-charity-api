0a21952fb26c3b9245131695122c2e55b2f0333a
Vishal101022 <raivishal939@gmail.com>
2024-11-19 22:20:58 +0530

    Add middleware & routes

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./util/db");
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+// routes
+const user = require("./routes/user");
+
+// models
+
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+  method: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+const app = express();
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use("/api/users", user);
+
+async function testConnection() {
+  try {
+    await db.authenticate();
+    console.log("Connection has been established successfully.");
+    await db.sync();
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+testConnection();
+
+app.listen(port, () => {
+  console.log("Server started on port 3000");
+});
