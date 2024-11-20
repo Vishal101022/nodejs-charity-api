9504300659bb623f20fc4cda680b9bc563a2d037
Vishal101022 <raivishal939@gmail.com>
2024-11-21 00:46:04 +0530

    Add charity reject route

--- routes/charity.js
+++ routes/charity.js
@@ -5,6 +5,7 @@ const auth = require("../middleware/auth");
 
 router.post("/register", charity.register);
 router.post("/approve/:id", auth.adminMiddleware, charity.approveCharity);
+router.post("/reject/:id", auth.adminMiddleware, charity.rejectCharity);
 router.post("/login", charity.login);
 router.get("/charity", charity.getCharity);
 router.get("/charity/:id", charity.getCharityById);
