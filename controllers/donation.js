a8d3ed2b9b87d2240b16eddb42b9c4434ea5ce64
Vishal101022 <raivishal939@gmail.com>
2024-11-20 20:26:37 +0530

    :sparkles: integrate razorpay payment api

--- controllers/donation.js
+++ controllers/donation.js
@@ -1,17 +1,104 @@
 const Donation = require("../models/donation");
+const Razorpay = require("razorpay");
+const Project = require("../models/project");
+const sequelize = require("../util/db");
+const dotenv = require("dotenv");
+const {
+  validateWebhookSignature,
+} = require("razorpay/dist/utils/razorpay-utils");
+dotenv.config();
 
-exports.getDonation = async (req, res) => { 
-    try {
-      const userId = req.user.id; 
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
 
-      const donations = await Donation.findAll({
-        where: { userId },
-        attributes: ["id", "amount", "charityName", "createdAt"],
-        order: [["createdAt", "DESC"]],
-      });
+exports.createDonation = async (req, res) => {
+  const { amount, projectId } = req.body;
+  const user = req.user.id;
+  const t = await sequelize.transaction();
 
-      res.status(200).json({ userId, donations });
-    } catch (error) {
-      res.status(500).json({ message: "Server error", error: error.message });
+  try {
+    const Order = await razorpay.orders.create({
+      amount: amount ,
+      currency: "INR",
+  
+    });
+
+    await Donation.create(
+      {
+        amount,
+        projectId,
+        userId: user,
+        status: "pending",
+        order_id: Order.id,
+      },
+      { transaction: t }
+    );
+    await t.commit();
+    res.status(200).json({
+      message: "Donation created successfully", Order});
+  } catch (error) {
+    await t.rollback();
+    console.log(error);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+exports.updateTransaction = async (req, res) => {
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    req.body;
+
+  try {
+    // validate the payment signature
+    const isValidSignature = await validateWebhookSignature(
+      razorpay_order_id + "|" + razorpay_payment_id,
+      razorpay_signature,
+      process.env.RAZORPAY_KEY_SECRET
+    );
+
+    if (!isValidSignature) {
+      return res.status(400).json({ message: "Invalid signature" });
     }
-}
+
+    // find the order and update payment details
+    const order = await Donation.findOne({
+      where: { order_id: razorpay_order_id },
+    });
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+
+    order.payment_id = razorpay_payment_id;
+    order.status = "paid";
+    await order.save();
+
+    // update the current donations of the project
+    const currDonation = await Project.findOne({
+      where: { id: order.projectId },
+    });
+    currDonation.currentDonations += order.amount;
+    await currDonation.save();
+    res.status(200).json({ status: "success" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+exports.getDonation = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const donations = await Donation.findAll({
+      where: { userId },
+      attributes: ["id", "amount", "charityName", "createdAt"],
+
+      order: [["createdAt", "DESC"]],
+    });
+
+    res.status(200).json({ userId, donations });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
