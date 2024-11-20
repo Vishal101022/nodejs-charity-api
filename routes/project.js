47c3b0e6fb72a5fc18f7ca6a2f9baa49bc76a238
Vishal101022 <raivishal939@gmail.com>
2024-11-20 20:29:42 +0530

    Add fetch project  routes

--- routes/project.js
+++ routes/project.js
@@ -4,8 +4,8 @@ const project = require("../controllers/project");
 const auth = require("../middleware/auth");
 
 router.post("/create", auth.charityAuthMiddleware, project.createProject);
-router.get("/projects", auth.charityAuthMiddleware, project.getAllProjects);
-router.get("/projects/:id", auth.charityAuthMiddleware, project.getProjectById);
+router.get("/fetch", project.getAllProjects);
+router.get("/fetch/:id", project.getProjectById);
 router.patch("/update/:id", auth.charityAuthMiddleware, project.updateProject);
 
 module.exports = router;
